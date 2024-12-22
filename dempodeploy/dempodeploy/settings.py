5f00bc5ab7f6b189bb3ecd731914aa6038c66aa2
Bhavin Chavada <bhavinchavada242846@gmail.com>
2024-12-22 13:29:02 +0530

    add comment about dababase

--- dempodeploy/dempodeploy/settings.py
+++ dempodeploy/dempodeploy/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
 import dj_database_url
 from decouple import config
 
+# railway postgresql db config
 DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 # connect db to aws prostgre
